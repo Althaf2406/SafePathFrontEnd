fe4ae9645d4add4dad0eced83c119fc61a7995f8
Althaf 2406 <malthafhilmi@student.ciputra.ac.id>
2026-06-03 15:53:56 +0700

    feat: implement watchOS app extension with live sync via WatchConnectivity

---
+++ SafePathWatch Watch App/SafePathWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafePathWatchApp.swift
+//  SafePathWatch Watch App
+//
+//  Created by student on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SafePathWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
