7587cff8c002b91f0ca44b83eb3c3a8c5ea2407e
Althaf2406 <malthafhilmi@student.ciputra.ac.id>
2026-06-10 17:29:04 +0700

    feat: implement WatchOS UI and WatchConnectivity manager for SOS and status updates

---
+++ SafePathWatchApp Watch App/iOSConnectivityManager.swift
@@ -0,0 +1,40 @@
+import Foundation
+import WatchConnectivity
+
+class iOSConnectivityManager: NSObject, WCSessionDelegate, ObservableObject {
+    var session: WCSession
+    
+    @Published var latestAlert: String = ""
+    
+    init(session: WCSession = .default) {
+        self.session = session
+        super.init()
+        session.delegate = self
+        session.activate()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+    
+    func sendActionToiOS(action: String, data: [String: Any]? = nil) {
+        if session.isReachable {
+            var payload: [String: Any] = ["action": action]
+            if let data = data {
+                payload["data"] = data
+            }
+            session.sendMessage(payload, replyHandler: nil) { error in
+                print("Error sending message: \(error.localizedDescription)")
+            }
+        } else {
+            print("Session is not reachable!")
+        }
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        DispatchQueue.main.async {
+            if let alert = message["alert"] as? String {
+                self.latestAlert = alert
+            }
+        }
+    }
+}
